bce72d9b169139f2a31b03340bb803c436506ab2
didi <didi@didideMBP.lan>
2021-06-10 01:53:19 +0800

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM alpine:latest
+
+WORKDIR /xray
+
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+
+RUN apk add nginx openssl
+
+COPY env.sh install.sh self_signature.sh server.json run.sh /xray/
+COPY ./nginx.conf /etc/nginx/conf.d/default.conf
+
+RUN sh install.sh
+
+EXPOSE 443 80
+
+CMD [ "sh", "/xray/run.sh" ]
---
+++ self_signature.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+TARGET=$1
+
+if [[ "$TARGET" == "" ]]
+then
+    TARGET="./"
+fi
+
+mkdir -p $TARGET
+
+echo "certificate build start in $TARGET"
+
+openssl req -newkey rsa:2048 -nodes -keyout $TARGET/self_key.pem -x509 -days 3650 -subj "/C=HK/ST=Tuen Mun District/L=Tuen Mun District/O=Crowley/OU=Crowley Software/CN=Crowley Admin/emailAddress=admin@crowley.com" -out $TARGET/self_cert.pem
+
+ln -s $TARGET/self_cert.pem $TARGET/cert
+ln -s $TARGET/self_key.pem $TARGET/key
+
+echo "certificate build done"
