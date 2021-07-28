7f6200026420c6ec4bd885f27181e2c25bf684bc
xiaoshao97 <ikkcss@hotmail.com>
2021-07-28 16:10:13 +0800

    buildx

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ then
     TAG="latest"
 fi
 
-docker build -t xiaoshao97/xray-server:$TAG $PWD
+docker buildx build --platform linux/amd64,linux/arm64 -t xiaoshao97/xray-server:$TAG $(cd $(dirname $0) && pwd -P) --push
